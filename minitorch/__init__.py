c9be56e627c1228bab1f9fcec07c00977280828a
Ebby <ebby.dd@gmail.com>
2021-11-11 14:51:25 +0800

    Fix a style error
    Signed-off-by: Ebby <ebby.dd@gmail.com>

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -4,4 +4,3 @@ from .module import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
-from .module import *  # noqa: F401,F403
