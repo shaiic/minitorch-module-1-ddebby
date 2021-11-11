18358e267b529d2bd230ae32b575895a20aef4c1
Ebby <ebby.dd@gmail.com>
2021-11-11 14:52:36 +0800

    Fix style error

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -3,4 +3,6 @@ from .scalar import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
-from .testing import *  # noqa: F401,F403
+from .testing import *  # noqa: F401,F403:wq:wq
+:wq
+
